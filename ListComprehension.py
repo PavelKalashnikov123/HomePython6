8dc1f46f684fc158b7f68703e7523186dda36230
PavelKalashnikov123 <pavel-kalashnikov@mail.ru>
2022-10-13 21:03:39 +0300

    task6

---
+++ ListComprehension.py
@@ -0,0 +1,20 @@
+                    # Пример 1
+# nums = [n for n in range(1,6)]
+# print(nums)
+
+                    # Пример 2
+
+# nums = [1, 2, 3, 4, 5]
+# squares = [n*n for n in nums]
+# print(squares) 
+
+                    # Пример 3
+
+# nums = [1, 2, 3, 4, 5]
+# odd_squares = [n*n for n in nums if n%2 == 1]
+# print(odd_squares)
+
+                    # Пример 4
+
+matrix = [[x for x in range(1, 4)] for y in range(1, 4)]
+print(matrix)
